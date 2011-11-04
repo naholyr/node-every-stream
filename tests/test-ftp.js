70b2982e56b26b45a6c583e1acc6e56990c3f853
Nicolas Chambrier <naholyr@gmail.com>
2011-11-04 23:48:40 +0100

    Added write support for FTP driver + tests (manually enable, providing a valid writable FTP server) TODO better handling of inner stream creation, current implementation make multiple calls to "create*()" occur...

--- tests/test-ftp.js
+++ tests/test-ftp.js
@@ -4,19 +4,40 @@ var fs = require('fs')
 
 function execute (Stream, cb) {
 
-  var ftp = new Stream('ftp://anonymous:fake%40email.com@ftp.free.fr/pub/assistance/demenagement.pdf')
-    , file = new Stream(path.resolve('dest-ftp-0.pdf'))
-  ftp.on('error', cb).pipe(file)
-  file.on('error', cb).on('close', function (err) {
-    try {
-      content = fs.readFileSync(file.path)
-      fs.unlinkSync(file.path)
-      assert.notEqual(content.toString(), '')
-    } catch (e) {
-      return cb(e)
-    }
-    return cb()
-  })
+  function test_read () {
+    var ftp = new Stream('ftp://anonymous:fake%40email.com@ftp.free.fr/pub/assistance/demenagement.pdf')
+      , file = new Stream(path.resolve('dest-ftp-0.pdf'))
+    ftp.on('error', cb).pipe(file)
+    file.on('error', cb).on('close', function (err) {
+      try {
+        content = fs.readFileSync(file.path)
+        fs.unlinkSync(file.path)
+        assert.notEqual(content.toString(), '')
+      } catch (e) {
+        return cb(e)
+      }
+      return cb()
+    })
+  }
+
+  function test_write () {
+    var ftp = new Stream('ftp://freebox:PASSWORD@mafreebox.freebox.fr/Disque%20dur/dst-ftp-0.txt')
+      , file = new Stream(path.resolve('src-ftp-0.txt'))
+    file.write('Hello, world').on('error', cb).pipe(ftp)
+    ftp.on('error', cb).on('success', function (err) {
+      ftp.end()
+      if (err) return cb(err)
+      try {
+        fs.unlinkSync(file.path)
+      } catch (e) {
+        return cb(e)
+      }
+      return cb()
+    })
+  }
+
+  test_read()
+  //test_write()
 
 }
 
