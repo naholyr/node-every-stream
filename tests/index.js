9cea74e6befc008bd5078f32e09b7d8384bd2cf6
Nicolas Chambrier <naholyr@gmail.com>
2011-11-04 21:31:20 +0100

    Added readonly support for "ftp://" streams

--- tests/index.js
+++ tests/index.js
@@ -5,7 +5,7 @@ var path = require('path')
 fs.readdir(__dirname, function (err, files) {
   files.forEach(function (file) {
     filename = path.resolve(__dirname, file)
-    if (filename != __filename && file.match(/\.js$/)) {
+    if (filename != __filename && file.match(/^test-.*\.js$/)) {
       require(filename)(Stream, function (err) {
         if (!err) console.log('[ OK ] %s', file)
         else {
